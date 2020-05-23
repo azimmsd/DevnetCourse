010c221987009ad8c2b58cf29a14ffbd45f6fc47
azimmsd <azimmasoudi@gmail.com>
2020-05-23 13:57:36 +0430

    jsonsample.py and NameSample.json added

---
+++ jsonsample.py
@@ -0,0 +1,22 @@
+import json
+jsonstr="""{
+  "people": [
+    {
+      "Id": "1",
+      "FirstName": "Ehsan",
+      "LastName": "Masoudi"
+    },
+    {
+      "Id": "2",
+      "FirstName": "Mehrnoosh",
+      "LastName": "Entezami"
+    },
+    {
+      "Id": "3",
+      "FirstName": "Iman",
+      "LastName": "Rajabizadeh"
+    }
+  ]
+}"""
+jsonobj=json.loads(jsonstr)
+print(jsonobj)
